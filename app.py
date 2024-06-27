5f564baac4517cd14b5d5b0c09c98b73c42e2258
Anand <jeevaanand93002@gmail.com>
2024-06-27 14:03:10 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+def hello_world():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
+    
+    
+    
+ git init
+
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/jeeva-anand/action-repo.git
+git push -u origin main 
